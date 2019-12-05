381527da8348701d5ff889a81aba0ef18d87ec96
JuYoung <op1700@nexol.co.kr>
2019-12-05 10:04:07 +0900

    네이버 영화순위 크롤링 프로그램

---
+++ NaverMovie_Crawl/NaverMovie_Crawl/Main.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace boardTest
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lv_boxoffice = new System.Windows.Forms.ListView();
+            this.lv_num = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lv_name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lv_level = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lv_openDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lv_daily = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lv_stack = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lv_nowmovie = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lv_boxoffice
+            // 
+            this.lv_boxoffice.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lv_num,
+            this.lv_name,
+            this.lv_level,
+            this.lv_openDate,
+            this.lv_daily,
+            this.lv_stack});
+            this.lv_boxoffice.GridLines = true;
+            this.lv_boxoffice.HideSelection = false;
+            this.lv_boxoffice.Location = new System.Drawing.Point(30, 10);
+            this.lv_boxoffice.Name = "lv_boxoffice";
+            this.lv_boxoffice.Size = new System.Drawing.Size(623, 250);
+            this.lv_boxoffice.TabIndex = 0;
+            this.lv_boxoffice.UseCompatibleStateImageBehavior = false;
+            this.lv_boxoffice.View = System.Windows.Forms.View.Details;
+            // 
+            // lv_num
+            // 
+            this.lv_num.Text = "번호";
+            this.lv_num.Width = 36;
+            // 
+            // lv_name
+            // 
+            this.lv_name.Text = "영화제목";
+            this.lv_name.Width = 100;
+            // 
+            // lv_level
+            // 
+            this.lv_level.Text = "관람등급";
+            this.lv_level.Width = 90;
+            // 
+            // lv_openDate
+            // 
+            this.lv_openDate.Text = "개봉날짜";
+            this.lv_openDate.Width = 90;
+            // 
+            // lv_daily
+            // 
+            this.lv_daily.Text = "일간관람객";
+            this.lv_daily.Width = 80;
+            // 
+            // lv_stack
+            // 
+            this.lv_stack.Text = "누적관람객";
+            this.lv_stack.Width = 80;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(287, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "네이버 영화 크롤링";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 81);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(696, 304);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.lv_boxoffice);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(688, 278);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "박스오피스 흥행순위";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.lv_nowmovie);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(688, 278);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "현재 상영영화";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // lv_nowmovie
+            // 
+            this.lv_nowmovie.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader7,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.lv_nowmovie.GridLines = true;
+            this.lv_nowmovie.HideSelection = false;
+            this.lv_nowmovie.Location = new System.Drawing.Point(31, 22);
+            this.lv_nowmovie.Name = "lv_nowmovie";
+            this.lv_nowmovie.Size = new System.Drawing.Size(621, 226);
+            this.lv_nowmovie.TabIndex = 1;
+            this.lv_nowmovie.UseCompatibleStateImageBehavior = false;
+            this.lv_nowmovie.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "번호";
+            this.columnHeader1.Width = 36;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "영화제목";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "개봉날짜";
+            this.columnHeader3.Width = 90;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "네티즌평점";
+            this.columnHeader4.Width = 90;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "예매율";
+            this.columnHeader5.Width = 80;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "상영시간";
+            this.columnHeader6.Width = 80;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "관람등급";
+            this.columnHeader7.Width = 100;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 393);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label1);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lv_boxoffice;
+        private System.Windows.Forms.ColumnHeader lv_num;
+        private System.Windows.Forms.ColumnHeader lv_name;
+        private System.Windows.Forms.ColumnHeader lv_level;
+        private System.Windows.Forms.ColumnHeader lv_openDate;
+        private System.Windows.Forms.ColumnHeader lv_daily;
+        private System.Windows.Forms.ColumnHeader lv_stack;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ListView lv_nowmovie;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+    }
+}
---
+++ NaverMovie_Crawl/NaverMovie_Crawl/Main.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace boardTest
+{
+    public partial class Main : Form
+    {
+        Loading loading = new Loading();
+        public Main()
+        {
+            InitializeComponent();   
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            loading.Show();
+            var options = new ChromeOptions();
+            options.AddArgument("headless");
+
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+            //박스오피스흥행순위
+            using (var boxoffice = new ChromeDriver(service, options))
+            {
+                boxoffice.Navigate().GoToUrl("http://www.naver.com");
+                
+
+                var retrieve = boxoffice.FindElement(By.Id("query"));
+                retrieve.SendKeys("박스오피스 흥행순위");
+                boxoffice.FindElement(By.Id("search_btn")).Click();
+
+                for (var num = 1; num < 9; num++)
+                {
+                    var t = "li[" + num + "]";
+                    var name = boxoffice.FindElement(
+                        By.XPath("//*[@id=\"main_pack\"]/div[1]/div/div[2]/div/div[1]/div[2]/div[1]/ul/" + t));
+
+                    string test = name.Text;
+                    Console.WriteLine(test);
+
+                    var inList = name.Text.Split('\n');
+
+                    lv_boxoffice.BeginUpdate();
+                    ListViewItem item;
+
+                    item = new ListViewItem(inList[1]);
+                    item.SubItems.Add(inList[2]);
+                    item.SubItems.Add(inList[0]);
+                    item.SubItems.Add(inList[4]);
+                    item.SubItems.Add(inList[6]);
+                    item.SubItems.Add(inList[8]);
+
+                    lv_boxoffice.Items.Add(item);
+
+                    lv_boxoffice.EndUpdate();
+                    Console.WriteLine("================");
+                }
+            }
+
+            //현재상영영화
+            using (var nowmovie = new ChromeDriver(service, options))
+            {
+                nowmovie.Navigate().GoToUrl("https://movie.naver.com/movie/running/current.nhn#");
+                nowmovie.Manage().Window.Maximize();
+
+                var movieCnt = nowmovie.FindElement(By.XPath("//*[@id=\"content\"]/div[1]/div[1]/div[3]/ul"))
+                    .FindElements(By.TagName("li")).Count;
+                for (var num = 1; num <= movieCnt; num++)
+                {
+
+                    var t = "li[" + num + "]";
+                    var name = nowmovie.FindElement(
+                        By.XPath("//*[@id=\"content\"]/div[1]/div[1]/div[3]/ul/" + t));
+
+                    string test = name.Text;
+                    //Console.WriteLine(test);
+
+                    var inList = name.Text.Split('\n');
+                    var list = new List<string>();
+                    for (var i = 0; i < inList.Length; i++)
+                    {
+                        list.Add(inList[i]);
+                        Console.WriteLine(list[i]);
+                    }
+
+                    //예매율이 없는경우
+                    if (!inList[5].Contains("예매율"))
+                    {
+                        list.Insert(4, "|");
+                        list.Insert(5, "예매율");
+                        list.Insert(6, "0");
+                        list.Insert(7, "%");
+                    }
+
+                    //관람등급이 없는경우 제목도 봐야함
+                    var textSplit = list[0].Split(' ');
+                    var textSplit2 = list[9].Split('|');
+                    var movieLevel = "";
+                    var movieTitle = "";
+                    if (!inList[0].Contains("관람"))
+                    {
+                        list.Insert(1, "미정");
+                        movieLevel = list[1];
+                        movieTitle = list[0];
+                    }
+                    else
+                    {
+                        movieLevel = textSplit[0] + textSplit[1];
+                        for (var i = 2; i < textSplit.Length; i++)
+                        {
+                            movieTitle += textSplit[i];
+                        }
+                    }
+
+                    //장르가 없는경우
+                    var genre = 2;
+                    if (textSplit2.Length == 2)
+                    {
+                        genre = 1;
+                    } 
+                    var movieOpenDate = textSplit2[genre].Replace("개봉", "");
+                    var movieTime = textSplit2[genre-1];
+
+                    lv_nowmovie.BeginUpdate();
+                    ListViewItem item;
+
+                    item = new ListViewItem(num.ToString());
+                    item.SubItems.Add(movieTitle);
+                    item.SubItems.Add(movieLevel);
+                    item.SubItems.Add(movieOpenDate);
+                    item.SubItems.Add(list[2]);
+                    item.SubItems.Add(list[6] + "%");
+                    item.SubItems.Add(movieTime);
+
+                    lv_nowmovie.Items.Add(item);
+
+                    lv_nowmovie.EndUpdate();
+                    Console.WriteLine("================");
+                }
+                loading.Hide();
+            }
+        
+            
+            #region 크롬브라우저 보이는 코드
+            //                        IWebDriver driver = new ChromeDriver();
+            //                 
+            //                        driver.Navigate().GoToUrl("http://www.naver.com");
+            //                        driver.Manage().Window.Maximize();
+            //            
+            //                        //검색창 input id = query
+            //                        //IWebElement retrieve = driver.FindElement(By.Id("query"));
+            //                        var retrieve = driver.FindElement(By.Id("query"));
+            //                        retrieve.SendKeys("박스오피스 흥행순위");
+            //                        driver.FindElement(By.Id("search_btn")).Click();
+            //                        Thread.Sleep(5000);
+            //            
+            //                        for (var num = 1; num < 9; num++)
+            //                        {
+            //                            var t = "li[" + num + "]";
+            //                            var movie = driver.FindElement(By.ClassName("contents03"));
+            //                            var name = movie.FindElement(
+            //                                By.XPath("//*[@id=\"main_pack\"]/div[1]/div/div[2]/div/div[1]/div[2]/div[1]/ul/" + t));
+            //            
+            //                            string test = name.Text;
+            //                            Console.WriteLine(test);
+            //                            Console.WriteLine("================");
+            //                        } 
+            //
+            //
+            //
+            //            driver.Close();
+            #endregion
+        }
+    }
+}
